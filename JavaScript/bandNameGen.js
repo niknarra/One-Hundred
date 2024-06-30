7b3a064560cb5a6590abe12bbd15f1b05080f97a
Nikhil Narra <nikhilnarra@vt.edu>
2024-06-30 20:04:36 +0530

    adding JS files

---
+++ JavaScript/bandNameGen.js
@@ -0,0 +1,9 @@
+//const process = require('process'); 
+
+console.log("Welcome to Band Name Generator!");
+
+let cityName = process.argv[2];
+
+let petName = process.argv[3];
+
+console.log(`Your band name could be ${cityName} ${petName}`); 
